8722a85c00bb6549a7a056eb2363fe4bcac6e167
GorHovhannisyan0001 <hovhannisyangor.2006@gmail.com>
2024-02-25 20:33:07 +0400

    add navigation

---
+++ app/navigation/menu.tsx
@@ -0,0 +1,30 @@
+import classes from "./menu.module.css";
+import { FC } from "react";
+import Link from "next/link";
+
+const Menu: FC = () => {
+  return (
+    <div className={classes["container"]}>
+      <Link className={classes["nav_link_first"]} href={"/"}>
+        ԳԼԽԱՎՈՐ
+      </Link>
+      <Link className={classes["nav_link"]} href={"/"}>
+        ՈՒՂԵՐՁՆԵՐ
+      </Link>
+      <Link className={classes["nav_link"]} href={"/"}>
+        ՄՇԱԿՈՒՅԹ ԵՎ ՍՊՈՐՏ
+      </Link>
+      <Link className={classes["nav_link"]} href={"/"}>
+        ԸՆԴՈՒՆԵԼՈՒԹՅՈՒՆ
+      </Link>
+      <Link className={classes["nav_link"]} href={"/"}>
+        ՍՈՑԻԱԼԱԿԱՆ ՎՈԼՈՐՏ
+      </Link>
+      <Link className={classes["nav_link"]} href={"/"}>
+        ԲԱՐԵԿԱՐԳՈՒՄ ԵՎ ԿՈՄՈՒՆԱԼ ՍՊԱՍԱՐԿՈՒՄ
+      </Link>
+    </div>
+  );
+};
+
+export default Menu;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
-    let num = 5;
+
 
     return (
         <main className={styles.main}>
-            <h2>hello git </h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est possimus consequatur
-                obcaecati in assumenda dolorum, debitis dicta id odio atque porro a totam distinctio
-                officiis sed sequi magnam provident nam?
-            </p>
+            
         </main>
     );
 }
