e505097966750f558217620228c82947b4dacff9
MarinaMkhitaryan <marina.mkhitaryan.2019@gmail.com>
2024-03-05 20:26:35 +0400

    add all changes

--- app/about/components/about.tsx
+++ app/about/components/about.tsx
@@ -1,38 +1,54 @@
-import {FC} from "react";
-// import images from "../../../public.img1"
+import {FC, useState} from "react";
+import images from "./about"
+import classes from "../style/about.module.scss"
+import Image from "next/image"
+import { GrLinkedin } from "react-icons/gr";
+import { FaFacebook } from "react-icons/fa";
 
 
 
+        
+        
 const BirthInfo: FC = () =>{
     
     return (
-        <div>
-           
+        <section>
+          
             
-            <div>
-                <section >
-                    <div>
-                <h1>ԿԱՌԼԵՆ ՍԱՄՎԵԼԻ ՄԿՐՏՉՅԱՆ</h1>
-                <h2>Ծնունդ</h2>
+            <div  className={classes["big_div"]}>
+                    
+                <h1 className={classes["name_container"]}>ԿԱՌԼԵՆ ՍԱՄՎԵԼԻ ՄԿՐՏՉՅԱՆ</h1>
+
+                <section className={classes["links_container"]} >
+                    <ul className={classes["socisl_block"]} >
+                        <li>
+                        <a href="" target="_blank" title="follow me on Facebook" >
+                        <FaFacebook className={classes["link_icon"]}/>
+                       </a>
+                        </li>
+                        <li >
+                            <a href="" target="_blank" title="follow me on Linkedin" >
+                            <GrLinkedin className={classes["link_icon"]}/> 
+                      </a>
+             </li>
+                    </ul>
+                </section>
+                <div className={classes["img_container"]}>
+    <Image src ="/about/image2.jpg" className={classes["about_img"]} alt="image2"  width={400} height={600} />
+    
+    </div>
+    <section>
+                <h4>Ծնունդ</h4>
                 <p>Ծնվել է 1986 թվականի դեկտեմբերի 21-ին, Արարատի մարզի Արտաշատ քաղաքում:
 2004-2006 թթ. ծառայել է ՀՀ Զինված ուժերում՝ որպես ջոկի հրամանատար:</p>
 
-</div>
-</section>
 
-<section>
-<div>
-<h1>ԿՐԹՈՒԹՅՈՒՆ</h1>
+
+    <h4>ԿՐԹՈՒԹՅՈՒՆ</h4>
 <p>2003 թվականին ավարտել է Արտաշատ քաղաքի թիվ 3  միջնակարգ դպրոցը:
 2004-2011 թթ. սովորել և ավարտել է Երևանի Խ. Աբովյանի անվան հայկական պետական մանկավարժական համալսարանի «Հոգեբանության և սոցիալական մանկավարժության»  ֆակուլտետը, ստացել է մանկավարժության  բակալավրի աստիճան: 2020 թվականից ընդունվել և սովորում է  Երևանի  Մ. Խորենացու անվան պետական համալսարանի  «Իրավաբանական»  ֆակուլտետում:</p>
-</div>
-</section>
-
-<section>
-<div>
-
-            <h1>ԱՇԽԱՏԱՆՔԱՅԻՆ ԳՈՐԾՈՒՆԵՈՒԹՅՈՒՆ</h1>
-            <p>2008-2009  թթ. աշխատել է ՀՀ ԱՆ քրեկատարողական հաստատությունում՝ որպես ՔԿՀ ԱԱՀ 2-րդ կարգի մասնագետ:
+   <h4>ԱՇԽԱՏԱՆՔԱՅԻՆ ԳՈՐԾՈՒՆԵՈՒԹՅՈՒՆ</h4>
+<p>2008-2009  թթ. աշխատել է ՀՀ ԱՆ քրեկատարողական հաստատությունում՝ որպես ՔԿՀ ԱԱՀ 2-րդ կարգի մասնագետ:
 2009-2011 թթ.  աշխատել է «Սանարե ֆարմ» ՍՊԸ-ում:
 2012-2015 թթ. զբաղվել է ձեռնարկատիրական գործունեությամբ ՌԴ-ում:
 2016-2019 թթ. աշխատել է «Սանարե ֆարմ» ՍՊԸ-ում:
@@ -42,20 +58,19 @@ const BirthInfo: FC = () =>{
 2021թ. դեկտեմբերի 5-ի  Արտաշատ խոշորացված համայնքի ավագանու ընտրություններից հետո ստանձնել է համայնքի ղեկավարի լիազորությունները:</p>
            
 
-            <p>Աշխատանքային գործունեության ընթացքում արժանացել է պատվոգրերի և շնորհակալագրերի:
+ <p>Աշխատանքային գործունեության ընթացքում արժանացել է պատվոգրերի և շնորհակալագրերի:
 «Քաղաքացիական պայմանագիր» կուսակցության անդամ է:
 Ամուսնացած է, ունի 2 որդի և մեկ դուստր:</p>
-</div>
+
 </section>
             
-            </div>
-
-             </div>
-           
+            
+</div>
+             
+</section>
                  
           
-          
-    )
+ )         
 };
 
 export default BirthInfo;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,10 @@ import BirthInfo from "./components/about";
 
 const About:FC=()=>{
     return <div>
-            <h2>hello government</h2>
+            <h2>About</h2>
             <BirthInfo/>
+            
+            
     </div>
 };
-export default About
+export default About;
