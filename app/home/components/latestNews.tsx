53d1706b969caf3d89b80416f5cbd1a1636b97bc
GorHovhannisyan0001 <hovhannisyangor.2006@gmail.com>
2024-03-12 18:21:19 +0400

    change latestnews second-a style

--- app/home/components/latestNews.tsx
+++ app/home/components/latestNews.tsx
@@ -4,19 +4,33 @@ import { news } from "./news";
 import Image from "next/image";
 
 const LatestNews: FC = () => {
-  const newsList = news.map(({ title, description,src,title2,description2 }, index) => {
-    return (
-      <div className={classes["newslist-container"]} key={index}>
-        <a href="/" className={classes["a"]}>
-           {src&& <Image src={src} alt="img" width={250} height={150} className={classes["news_image"]}/>}
-          <p className={classes["title_p"]}>{title}</p>
-          <p className={classes["description_p"]}>{description}</p>
-          {title2&& <p className={classes["title_p_2"]}>{title2}</p>}
-          {description2&& <p className={classes["description_p"]}>{description2}</p>}
-        </a>
-      </div>
-    );
-  });
+  const newsList = news.map(
+    ({ title, description, src, title2, description2 }, index) => {
+      return (
+        <div className={classes["newslist-container"]} key={index}>
+          <a href="/" className={classes["a"]}>
+            {src && (
+              <Image
+                src={src}
+                alt="img"
+                width={250}
+                height={150}
+                className={classes["news_image"]}
+              />
+            )}
+            <p className={classes["title_p"]}>{title}</p>
+            <p className={classes["description_p"]}>{description}</p>
+          </a>
+          <a href="/" className={classes["a"]}>
+            {title2 && <p className={classes["title_p_2"]}>{title2}</p>}
+            {description2 && (
+              <p className={classes["description_p"]}>{description2}</p>
+            )}
+          </a>
+        </div>
+      );
+    }
+  );
   return (
     <div className={classes["container"]}>
       <div className={classes["news-title-div"]}>
