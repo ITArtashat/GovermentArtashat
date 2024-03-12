0f43d9f137ad492527f06b6830b528b14f219f02
gevorg <gevoryenokyan@gmail.com>
2024-03-12 20:34:23 +0400

    add respaonsive

--- app/home/components/days.tsx
+++ app/home/components/days.tsx
@@ -3,16 +3,16 @@ import { FC } from "react";
 import { importantDays } from "./importantDays";
 
 const Days: FC = () => {
-  const imDays = importantDays.map((el, i) => {
-    return (
-      <div key={i} className={classes["days_item"]}>
-        <h2>{el.title}</h2>
-        <p className={classes["days_description"]}>{el.description}</p>
-      </div>
-    );
-  });
+    const imDays = importantDays.map((el, i) => {
+        return (
+            <div key={i} className={classes["days_item"]}>
+                <h2 className={classes["days_title"]}>{el.title}</h2>
+                <p className={classes["days_description"]}>{el.description}</p>
+            </div>
+        );
+    });
 
-  return <div className={classes["days_container"]}>{imDays}</div>;
+    return <div className={classes["days_container"]}>{imDays}</div>;
 };
 
 export default Days;
--- app/home/components/homepageContact.tsx
+++ app/home/components/homepageContact.tsx
@@ -3,40 +3,30 @@ import { FC } from "react";
 import Image from "next/image";
 
 const HomePageContact: FC = () => {
-  return (
-    <div className={classes["container"]}>
-      <div className={classes["child_container"]}>
-        <div className={classes["img_container"]}>
-          <Image
-            className={classes["contact_img"]}
-            src={"/images/home-page-images/contact.jpg"}
-            alt="image"
-            width={600}
-            height={250}
-          />
+    return (
+        <div className={classes["container"]}>
+            <div className={classes["child_container"]}>
+                <div className={classes["img_container"]}>
+                    <Image
+                        className={classes["contact_img"]}
+                        src={"/images/home-page-images/contact.jpg"}
+                        alt="image"
+                        width={600}
+                        height={250}
+                    />
+                </div>
+                <div className={classes["contact_container"]}>
+                    <h1>Կապ մեզ հետ</h1>
+                    <div className={classes["input_container"]}>
+                        <input type="text" placeholder="name" className={classes["inputs"]} />
+                        <input type="email" placeholder="email" className={classes["inputs"]} />
+                        <textarea className={classes["input_textarea"]}>Messages....</textarea>
+                    </div>
+                    <button className={classes["send_button"]}>Send Messages</button>
+                </div>
+            </div>
         </div>
-        <div className={classes["contact_container"]}>
-          <h1>Կապ մեզ հետ</h1>
-          <div className={classes["input_container"]}>
-            <input
-              type="text"
-              placeholder="name"
-              className={classes["inputs"]}
-            />
-            <input
-              type="email"
-              placeholder="email"
-              className={classes["inputs"]}
-            />
-            <textarea className={classes["input_textarea"]}>
-              Messages....
-            </textarea>
-          </div>
-          <button className={classes["send_button"]}>Send Messages</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePageContact;
